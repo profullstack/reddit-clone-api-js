f7ff87a1ada03ef83336b55c9e994cb59671134f
Anthony Ettinger <anthony@chovy.com>
2020-03-30 19:20:07 -0700

    cache

---
+++ cache.js
@@ -0,0 +1,15 @@
+const redis = require('redis');
+const { promisify } = require('util');
+const cache = redis.createClient(process.env.REDIS_URL);
+
+cache.on('connect', function() {
+  console.log('connected to redis: ', process.env.REDIS_URL);
+});
+
+module.exports = {
+  cache,
+  ...cache,
+  getAsync: promisify(cache.get).bind(cache),
+  setAsync: promisify(cache.set).bind(cache),
+  keysAsync: promisify(cache.keys).bind(cache),
+};
