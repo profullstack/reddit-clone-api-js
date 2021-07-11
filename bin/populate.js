026779abd5771aef68ec655eb38451a938df9769
Anthony Ettinger <anthony@chovy.com>
2021-07-11 15:48:26 -0700

    populate

--- bin/populate.js
+++ bin/populate.js
@@ -3,10 +3,11 @@
 require('dotenv').config();
 import fetch from 'node-fetch';
 
-async function start() {
+async function upvotocracy() {
   const recent = 'https://briskreader.com/api/1/recent';
   const res = await fetch(recent);
   const data = await res.json();
+
 
   for (let item of data) {
     console.log(item.shortId);
@@ -16,10 +17,10 @@ async function start() {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + process.env.AUTH_BRISKREADER,
+        Authorization: 'Bearer ' + process.env.AUTH_UPVOTOCRACY,
       },
       body: JSON.stringify({
-        category: process.env.BR_CAT_NEWS,
+        category: process.env.UPVOTOCRACY_CAT_NEWS,
         type: 'link',
         url: 'https://briskreader.com/link/' + item.shortId,
         hashtags,
@@ -32,4 +33,44 @@ async function start() {
   }
 }
 
-start();
+async function bitcoinlatte() {
+	const cryptos = [{
+		tag: 'BTC',
+		cat: process.env.BITCOINLATTE_CAT_BTC
+	},{
+		tag: 'ETH',
+		cat: process.env.BITCOINLATTE_CAT_ETH
+	}];
+
+	for (let tag of cryptos) {
+		const url = 'https://briskreader.com/api/1/cryptos/'+tag.tag;
+		const res = await fetch(url);
+		const data = await res.json();
+
+		for (let item of data) {
+			console.log(item.shortId);
+			const hashtags = item.summary.topics;
+
+			const res2 = await fetch('https://bitcoinlatte.com/api/1/posts', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + process.env.AUTH_BITCOINLATTE,
+				},
+				body: JSON.stringify({
+					category: tag.cat,
+					type: 'link',
+					url: 'https://briskreader.com/link/' + item.shortId,
+					hashtags,
+					title: item.title,
+					thumb: item.meta.thumb,
+				}),
+			});
+			const data2 = await res2.json();
+			console.log(data2);
+		}
+	}
+}
+
+bitcoinlatte()
+upvotocracy()
