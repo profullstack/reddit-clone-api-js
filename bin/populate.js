8dffd1de2244ce4fe7d890874821784d0924f3c5
Anthony Ettinger <anthony@chovy.com>
2021-07-12 10:59:51 -0700

    pop

--- bin/populate.js
+++ bin/populate.js
@@ -42,8 +42,42 @@ async function bitcoinlatte() {
 		cat: process.env.BITCOINLATTE_CAT_ETH
 	}];
 
+	const topics = [{
+		tag: 'cbdc',
+		cat: process.env.BITCOINLATTE_CAT_CBDC
+	}];
+
 	for (let tag of cryptos) {
 		const url = 'https://briskreader.com/api/1/cryptos/'+tag.tag;
+		const res = await fetch(url);
+		const data = await res.json();
+
+		for (let item of data) {
+			console.log(item.shortId);
+			const hashtags = item.summary.topics;
+
+			const res2 = await fetch('https://bitcoinlatte.com/api/1/posts', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + process.env.AUTH_BITCOINLATTE,
+				},
+				body: JSON.stringify({
+					category: tag.cat,
+					type: 'link',
+					url: 'https://briskreader.com/link/' + item.shortId,
+					hashtags,
+					title: item.title,
+					thumb: item.meta.thumb,
+				}),
+			});
+			const data2 = await res2.json();
+			console.log(data2);
+		}
+	}
+
+	for (let tag of topics) {
+		const url = 'https://briskreader.com/api/1/topics/'+tag.tag;
 		const res = await fetch(url);
 		const data = await res.json();
 
